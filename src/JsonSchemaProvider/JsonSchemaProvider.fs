afd0cfd3a56d13b23fe24e83b4f92a0e6a1847b3
Florian Lorenzen <florian.lorenzen@posteo.de>
2023-09-22 23:41:40 +0000

    Some things are already working

--- src/JsonSchemaProvider/JsonSchemaProvider.fs
+++ src/JsonSchemaProvider/JsonSchemaProvider.fs
@@ -1,81 +1,123 @@
 ﻿namespace JsonSchemaProvider
 
+open System.Collections.Generic
 open System.Reflection
 open FSharp.Core.CompilerServices
 open ProviderImplementation.ProvidedTypes
-
-type PPair(fst: string, snd: string) =
-    member __.Fst() = fst
-    member __.Snd() = snd
-
+open NJsonSchema
+open FSharp.Data
+open FSharp.Data.JsonExtensions
 
 [<TypeProvider>]
-type JsonSchemaProvider(config: TypeProviderConfig) as this =
+type JsonSchemaProviderImpl(config: TypeProviderConfig) as this =
     inherit TypeProviderForNamespaces(config)
     let namespaceName = "JsonSchemaProvider"
     let thisAssembly = Assembly.GetExecutingAssembly()
-    let staticParams = [ ProvidedStaticParameter("prefix", typeof<string>) ]
-    let baseTy = typeof<PPair>
+    let staticParams = [ ProvidedStaticParameter("schema", typeof<string>) ]
+    let baseTy = typeof<JsonValue>
 
-    let jsonTy =
-        ProvidedTypeDefinition(thisAssembly, namespaceName, "Pair", baseType = Some baseTy)
+    let jsonSchemaTy =
+        ProvidedTypeDefinition(thisAssembly, namespaceName, "JsonSchemaProvider", baseType = Some baseTy)
+
+    let rec generateSelectorsForObject
+        (ty: ProvidedTypeDefinition)
+        (props: IDictionary<string, JsonSchemaProperty>)
+        (requiredProps: ICollection<string>)
+        =
+        for prop in props do
+            let name = prop.Key
+            let propType = prop.Value.Type
+            let isRequired = requiredProps.Contains(name)
+            let returnType =
+                match propType with
+                | JsonObjectType.String -> if isRequired then typeof<string> else typeof<string option>
+                | JsonObjectType.Boolean -> if isRequired then typeof<bool> else typeof<bool option>
+                | JsonObjectType.Integer -> if isRequired then typeof<int> else typeof<int option>
+                | JsonObjectType.Number -> if isRequired then typeof<float> else typeof<float option>
+                | JsonObjectType.Object ->
+                    let innerTy =
+                        ProvidedTypeDefinition(thisAssembly, namespaceName, name, baseType = Some baseTy)
+
+                    generateSelectorsForObject innerTy prop.Value.Properties |> ignore
+                    ty.AddMember(innerTy)
+
+                    if requiredProps.Contains(name) then innerTy else innerTy
+                | _ -> failwith $"Unsupported type {propType}"
+
+            let accessor =
+                ProvidedProperty(
+                    propertyName = name,
+                    propertyType = returnType,
+                    getterCode =
+                        fun args ->
+                            <@@
+                                let jval = (%%args[0]: JsonValue).TryGetProperty(name)
+                                let unwrap jval = if isRequired then (Option.get jval):>obj else jval:>obj
+                                match propType with
+                                | JsonObjectType.String -> Option.map (fun (x:JsonValue )-> x.AsString()) jval |> unwrap
+                                // | JsonObjectType.Boolean -> Option.map (fun x -> x.AsBoolean():>obj) jval |> unwrap
+                                // | JsonObjectType.Integer -> Option.map (fun x -> x.AsInteger():>obj) jval |> unwrap
+                                // | JsonObjectType.Number -> Option.map (fun x -> x.AsFloat():>obj) jval |> unwrap
+                                // | JsonObjectType.Object -> jval: obj
+                                | _ -> failwith $"Unsupported type {propType}"
+                            @@>
+                )
+
+            ty.AddMember(accessor)
+
+        ty
 
     do
-        jsonTy.DefineStaticParameters(
+        jsonSchemaTy.DefineStaticParameters(
             parameters = staticParams,
             instantiationFunction =
                 fun typeName parameterValues ->
                     match parameterValues with
-                    | [| :? string as prefix |] ->
+                    | [| :? string as schemaSource |] ->
+                        let schema =
+                            JsonSchema.FromJsonAsync(schemaSource)
+                            |> Async.AwaitTask
+                            |> Async.RunSynchronously
+
                         let ty =
                             ProvidedTypeDefinition(thisAssembly, namespaceName, typeName, baseType = Some baseTy)
 
-                        let fst =
-                            ProvidedMethod(
-                                methodName = prefix + "Fst",
-                                parameters = [],
-                                returnType = typeof<string>,
-                                isStatic = false,
-                                invokeCode = fun args -> <@@ ((%%args[0]: PPair)).Fst() @@>
-                            )
+                        if schema.Type <> JsonObjectType.Object then
+                            failwith "Only object supported"
 
-                        let snd =
-                            ProvidedMethod(
-                                methodName = prefix + "Snd",
-                                parameters = [],
-                                returnType = typeof<string>,
-                                isStatic = false,
-                                invokeCode = fun args -> <@@ ((%%args[0]: PPair)).Snd() @@>
-                            )
+                        generateSelectorsForObject ty schema.Properties schema.RequiredProperties|> ignore
 
-                        ty.AddMember(fst)
-                        ty.AddMember(snd)
-
-                        let baz =
+                        let parse =
                             ProvidedMethod(
-                                methodName = "Baz",
-                                parameters = [],
+                                methodName = "Parse",
+                                parameters = [ ProvidedParameter("json", typeof<string>) ],
                                 returnType = ty,
                                 isStatic = true,
-                                invokeCode = fun args -> <@@ PPair("left" + prefix, "right" + prefix) @@>
+                                invokeCode =
+                                    fun args ->
+                                        <@@
+                                            let schema =
+                                                JsonSchema.FromJsonAsync(schemaSource)
+                                                |> Async.AwaitTask
+                                                |> Async.RunSynchronously
+
+                                            let result = schema.Validate((%%args[0]): string)
+
+                                            if Seq.isEmpty result then
+                                                JsonValue.Parse(%%args[0])
+                                            else
+                                                failwith "Invalid JSON"
+                                        @@>
                             )
 
-                        ty.AddMember(baz)
+                        ty.AddMember(parse)
 
-                        let ctor =
-                            ProvidedConstructor(
-                                parameters =
-                                    [ ProvidedParameter("fst", typeof<string>)
-                                      ProvidedParameter("snd", typeof<string>) ],
-                                invokeCode = fun args -> <@@ PPair((%%(args[0]): string), (%%(args[1]): string)) @@>
-                            )
-
-                        ty.AddMember(ctor)
                         ty
                     | _ -> failwith "unexpected paramter values"
         )
 
-    do this.AddNamespace(namespaceName, [ jsonTy ])
+    do this.AddNamespace(namespaceName, [ jsonSchemaTy ])
+
 
 [<assembly: TypeProviderAssembly>]
 do ()
--- tests/TryJsonProvider/Program.fs
+++ tests/TryJsonProvider/Program.fs
@@ -1,18 +1,110 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open JsonSchemaProvider
 
-printfn "Hello from F#"
+// printfn "Hello from F#"
 
-type FooPair = Pair<prefix="Fob">
+// type FooPair = Pair<prefix="Fob">
 
-let foo = FooPair("1", "2")
+// let foo = FooPair("1", "2")
 
-printfn "%s" (foo.FobFst())
+// printfn "%s" (foo.FobFst())
 
-printfn "%s" (foo.FobSnd())
+// printfn "%s" (foo.FobSnd())
 
-printfn "%s" (foo.FobSnd())
+// printfn "%s" (foo.FobSnd())
 
-let x = FooPair.Baz()
+// let x = FooPair.Baz()
 
-printfn "%s" (x.FobSnd())
+// printfn "%s" (x.FobSnd())
+
+
+// type Data = JsonProvider<schema="...">
+// Data.parse(input: string) -> Data
+// let d = Data.parse(...)
+// d.
+
+open Newtonsoft.Json.Linq
+open NJsonSchema
+[<Literal>]
+let schemaSource = """{
+  "$id": "https://example.com/address.schema.json",
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "description": "An address similar to http://microformats.org/wiki/h-card",
+  "type": "object",
+  "properties": {
+    "post-office-box": {
+      "type": "string"
+    },
+    "extended-address": {
+      "type": "string"
+    },
+    "street-address": {
+      "type": "string"
+    },
+    "locality": {
+      "type": "string"
+    },
+    "region": {
+      "type": "string"
+    },
+    "postal-code": {
+      "type": "string"
+    },
+    "country-name": {
+      "type": "string"
+    },
+    "nested": {
+        "type": "object",
+        "properties": {
+            "propa": { "type": "string"},
+            "propb": { "type": "string"}
+        }
+    },
+    "code": {
+        "type": "integer"
+    },
+    "again": { "type": "boolean"}
+  },
+  "required": [ "locality", "region", "country-name" ],
+  "dependentRequired": {
+    "post-office-box": [ "street-address" ],
+    "extended-address": [ "street-address" ]
+  }
+}"""
+
+let data = """{
+    "post-office-box": "foo",
+    "locality": "ff",
+    "region": "d",
+    "country-name": "ger",
+    "nested": {"propa": "AA", "propb": "BB"},
+    "code": 17,
+    "again": true
+}"""
+
+let data1 = """{
+    "post-office-box": "foo",
+    "locality": "ff",
+    "region": "d",
+}"""
+let schema = JsonSchema.FromJsonAsync(schemaSource) |> Async.AwaitTask |> Async.RunSynchronously
+let json = JToken.Parse(data)
+
+printfn "%O" (schema.Validate(json))
+
+let vali(data: string) =
+    let json =JToken.Parse(data)
+    let result = schema.Validate(json)
+    Seq.isEmpty result
+
+// printf "%b" (vali data)
+// printf "%s" (JToken.op_Explicit(json["post-office-box"]))
+printfn "%O" (JToken.op_Explicit(json["again"]):bool)
+type Addr = JsonSchemaProvider<schema=schemaSource>
+
+let d = Addr.Parse(data)
+
+printfn "%s" (d.region)
+printfn "%s" (d.nested.propa)
+printfn "%d" (d.code)
+printfn "%O" (d.again)
