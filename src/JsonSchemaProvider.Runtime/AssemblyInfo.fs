dbdb9e586f02092e6b25f614b0bb9a7c0fdf88ba
Florian Lorenzen <florian.lorenzen@posteo.de>
2023-10-17 21:19:18 +0200

    Bump version to 0.1.0-pre.1
    ## [0.1.0-pre.1] - 2023-10-17
    
    First release
    
    ### Added
    
    - Basic support for JSON Schemas with objects at the outer level.
    
    [Unreleased]: https://github.com/florenz/JsonSchemaProvider/compare/v0.1.0-pre.1...HEAD

--- src/JsonSchemaProvider.Runtime/AssemblyInfo.fs
+++ src/JsonSchemaProvider.Runtime/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("JsonSchemaProvider.Runtime")>]
 [<assembly: AssemblyProductAttribute("JsonSchemaProvider")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-10-16T00:00:00.0000000+02:00")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-10-17T00:00:00.0000000+02:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","pre")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","03b8fca22ec8b7ea48427c8d646f75a008b90636")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","087d30424ef146c40eb295e6febb3847a048179c")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "JsonSchemaProvider.Runtime"
     let [<Literal>] AssemblyProduct = "JsonSchemaProvider"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-10-16T00:00:00.0000000+02:00"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-10-17T00:00:00.0000000+02:00"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "pre"
-    let [<Literal>] AssemblyMetadata_GitHash = "03b8fca22ec8b7ea48427c8d646f75a008b90636"
+    let [<Literal>] AssemblyMetadata_GitHash = "087d30424ef146c40eb295e6febb3847a048179c"
