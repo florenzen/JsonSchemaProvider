81346b124c68a84dca897f1649ded7f8c9d36490
Florian Lorenzen <florian.lorenzen@posteo.de>
2024-03-25 17:15:29 +0000

    More on Expr generation

--- src/JsonSP.DesignTime/ExprGenerator.fs
+++ src/JsonSP.DesignTime/ExprGenerator.fs
@@ -25,3 +25,68 @@ namespace JsonSP.DesignTime
 
 module ExprGenerator =
     open FSharp.Quotations
+    open FSharp.Data
+    open SchemaRep
+    open JsonSchemaProvider
+
+    let rec private generateJsonValToTypedObject (fSharpType: FSharpType) : Expr =
+        match fSharpType with
+        | FSharpBool -> <@@ fun (jsonVal: JsonValue) -> jsonVal.AsBoolean() @@>
+        | FSharpClass(_) -> <@@ fun (jsonVal: JsonValue) -> NullableJsonValue(jsonVal) @@>
+        | FSharpList(innerType) ->
+            let generateForInner = generateJsonValToTypedObject innerType
+            <@@ fun (jsonVal: JsonValue) -> Array.map %%generateForInner (jsonVal.AsArray()) @@>
+        | FSharpDouble -> <@@ fun (jsonVal: JsonValue) -> jsonVal.AsFloat() @@>
+        | FSharpInt -> <@@ fun (jsonVal: JsonValue) -> jsonVal.AsInteger() @@>
+        | FSharpString -> <@@ fun (jsonVal: JsonValue) -> jsonVal.AsString() @@>
+
+    let rec private generateTypedObjectToJsonVal () : Expr = failwith "nyi"
+
+    let generatePropertyGetter
+        { Name = name
+          Optional = optional
+          FSharpType = fSharpType }
+        : Expr list -> Expr =
+        fun (args: Expr list) ->
+            let conversion = generateJsonValToTypedObject fSharpType
+
+            if optional then
+                <@@
+                    match ((%%args[0]): NullableJsonValue).JsonVal.TryGetProperty(name) with
+                    | None -> None
+                    | Some(jsonVal) -> Some((%%conversion) jsonVal)
+                @@>
+            else
+                <@@
+                    let jsonVal = ((%%args[0]): NullableJsonValue).JsonVal[name]
+                    (%%conversion) jsonVal
+                @@>
+
+    let generateCreateInvokeCode
+        (schemaHashCode: int32)
+        (schemaSource: string)
+        (properties: FSharpProperty list)
+        : Expr list -> Expr =
+        fun (args: Expr list) ->
+            let properties: Expr =
+                // [| for arg in args -> |]
+                failwith "nyi"
+
+            <@@
+                let record = NullableJsonValue(JsonValue.Record(Array.concat %%properties))
+                let recordSource = record.ToString()
+
+                let schema = SchemaCache.retrieveSchema schemaHashCode schemaSource
+
+                let validationErrors = schema.Validate(recordSource)
+
+                if Seq.isEmpty validationErrors then
+                    record
+                else
+                    let message =
+                        validationErrors
+                        |> Seq.map (fun validationError -> validationError.ToString())
+                        |> fun msgs -> System.String.Join(", ", msgs) |> sprintf "JSON Schema validation failed: %s"
+
+                    raise (System.ArgumentException(message, recordSource))
+            @@>
